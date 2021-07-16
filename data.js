ed9fc5a5610908936f2c42fac2c6607a7a1b3f78
denin-zt88 <denin.p@zohocorp.com>
2021-07-16 17:24:53 +0530

    new

--- data.js
+++ data.js
@@ -1,108 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-a_2---panorama",
-      "name": "a_2 - Panorama",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -0.030568037960433614,
-        "pitch": -0.012904738201918065,
-        "fov": 1.3333127936580627
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.5219439117256428,
-          "pitch": 0.21261928407302833,
-          "rotation": 0,
-          "target": "1-a_3---panorama"
-        },
-        {
-          "yaw": -1.109515504400104,
-          "pitch": 0.3035065254379141,
-          "rotation": 0,
-          "target": "4-a_6---panorama"
-        },
-        {
-          "yaw": 1.4268684722208453,
-          "pitch": 0.2929100158282161,
-          "rotation": 6.283185307179586,
-          "target": "6-a_8---panorama"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-a_3---panorama",
-      "name": "a_3 - Panorama",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -0.524585701862879,
-        "pitch": 0.10406778805639405,
-        "fov": 1.3333127936580627
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.769577199260419,
-          "pitch": 0.18820137024982309,
-          "rotation": 0,
-          "target": "0-a_2---panorama"
-        },
-        {
-          "yaw": -3.050775314286957,
-          "pitch": 0.1598171427938837,
-          "rotation": 0,
-          "target": "4-a_6---panorama"
-        },
-        {
-          "yaw": 1.976875381134005,
-          "pitch": 0.24547919904615512,
-          "rotation": 0,
-          "target": "6-a_8---panorama"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-a_4---panorama",
-      "name": "a_4 - Panorama",
+      "id": "0-dining-room",
+      "name": "Dining Room",
       "levels": [
         {
           "tileSize": 256,
@@ -130,17 +30,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 3.1269804051596672,
-          "pitch": 0.3382668936494575,
+          "yaw": 0.08628439634277818,
+          "pitch": 0.1670105591009463,
           "rotation": 0,
-          "target": "5-a_7---panorama"
+          "target": "1-dining-table"
+        },
+        {
+          "yaw": -1.7310886359964481,
+          "pitch": 0.5347713941843502,
+          "rotation": 0,
+          "target": "4-wash-counter"
+        },
+        {
+          "yaw": -1.5578262030453125,
+          "pitch": 0.11746789700056581,
+          "rotation": 0,
+          "target": "2-kitchen-view_02"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-a_5---panorama",
-      "name": "a_5 - Panorama",
+      "id": "1-dining-table",
+      "name": "Dining Table",
       "levels": [
         {
           "tileSize": 256,
@@ -168,29 +80,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.5461774374678114,
-          "pitch": 0.5560410503215216,
+          "yaw": -2.652915994746385,
+          "pitch": 0.2809792062350702,
           "rotation": 0,
-          "target": "5-a_7---panorama"
+          "target": "4-wash-counter"
         },
         {
-          "yaw": -1.5786588583431715,
-          "pitch": 0.6786874632723592,
+          "yaw": 3.134587763151817,
+          "pitch": 0.34646494182647203,
           "rotation": 0,
-          "target": "6-a_8---panorama"
-        },
-        {
-          "yaw": -1.295811501224824,
-          "pitch": 0.08283032336472118,
-          "rotation": 0,
-          "target": "1-a_3---panorama"
+          "target": "0-dining-room"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-a_6---panorama",
-      "name": "a_6 - Panorama",
+      "id": "2-kitchen-view_02",
+      "name": "Kitchen View_02",
       "levels": [
         {
           "tileSize": 256,
@@ -218,17 +124,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.124000882346138,
-          "pitch": 0.3428932425628979,
+          "yaw": -3.0679661622170205,
+          "pitch": 0.14828827757776608,
           "rotation": 0,
-          "target": "0-a_2---panorama"
+          "target": "5-work-area"
+        },
+        {
+          "yaw": 2.7561491257194533,
+          "pitch": 0.22484062983638253,
+          "rotation": 0,
+          "target": "3-kitchen-view_01"
+        },
+        {
+          "yaw": 1.9697749619954825,
+          "pitch": 0.24576945098153757,
+          "rotation": 0,
+          "target": "4-wash-counter"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-a_7---panorama",
-      "name": "a_7 - Panorama",
+      "id": "3-kitchen-view_01",
+      "name": "Kitchen View_01",
       "levels": [
         {
           "tileSize": 256,
@@ -256,35 +174,29 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.6845897834916208,
-          "pitch": 0.5529483508464441,
+          "yaw": -1.1122235175778208,
+          "pitch": 0.33515656390588333,
           "rotation": 0,
-          "target": "3-a_5---panorama"
+          "target": "5-work-area"
         },
         {
-          "yaw": -1.670819679643003,
-          "pitch": 0.3149061864737597,
+          "yaw": 0.546854258148894,
+          "pitch": 0.2686123658213422,
           "rotation": 0,
-          "target": "6-a_8---panorama"
+          "target": "2-kitchen-view_02"
         },
         {
-          "yaw": 0.10711065700879985,
-          "pitch": 0.17727476413601195,
+          "yaw": 1.4683797978882893,
+          "pitch": 0.3385178994915279,
           "rotation": 0,
-          "target": "2-a_4---panorama"
-        },
-        {
-          "yaw": -1.5486271523739426,
-          "pitch": 0.08561174155371809,
-          "rotation": 0,
-          "target": "1-a_3---panorama"
+          "target": "4-wash-counter"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-a_8---panorama",
-      "name": "a_8 - Panorama",
+      "id": "4-wash-counter",
+      "name": "Wash Counter",
       "levels": [
         {
           "tileSize": 256,
@@ -312,28 +224,66 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.49420969924170244,
-          "pitch": 0.795450406666081,
-          "rotation": 6.283185307179586,
-          "target": "3-a_5---panorama"
-        },
-        {
-          "yaw": 0.4886349450472238,
-          "pitch": 0.34072245604255613,
-          "rotation": 6.283185307179586,
-          "target": "5-a_7---panorama"
-        },
-        {
-          "yaw": -1.9306858442877335,
-          "pitch": 0.07772939792720557,
+          "yaw": 0.6163032282049414,
+          "pitch": 0.11163811197498674,
           "rotation": 0,
-          "target": "1-a_3---panorama"
+          "target": "1-dining-table"
+        },
+        {
+          "yaw": 1.6467120467068135,
+          "pitch": 0.5306747935463854,
+          "rotation": 0,
+          "target": "0-dining-room"
+        },
+        {
+          "yaw": -1.3369982671406149,
+          "pitch": 0.13695438778809788,
+          "rotation": 0,
+          "target": "2-kitchen-view_02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-work-area",
+      "name": "Work Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2241251474090706,
+          "pitch": 0.21553123262949647,
+          "rotation": 0,
+          "target": "3-kitchen-view_01"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Project 360",
   "settings": {
     "mouseViewMode": "qtvr",
     "autorotateEnabled": true,
